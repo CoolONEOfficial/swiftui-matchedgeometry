94fde2e2d6af279aebbcd63b5fe38f747bfb534d
Nickolay Trukhin <coolone.official@gmail.com>
2021-11-27 17:44:17 +0300

    Initial Commit

---
+++ matchedgeo/matchedgeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchedgeoApp.swift
+//  matchedgeo
+//
+//  Created by Nickolay Truhin on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct matchedgeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
